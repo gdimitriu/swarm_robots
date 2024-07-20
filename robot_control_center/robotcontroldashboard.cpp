b22b6dc7c42c7e09b9d92f8b3d64e3eaf8c6b304
gdimitriu <gabriel.dimitriu@yahoo.com>
2024-07-19 23:38:07 -0700

    added thread for robotControl UI

--- robot_control_center/robotcontroldashboard.cpp
+++ robot_control_center/robotcontroldashboard.cpp
@@ -6,6 +6,7 @@
 #include <QJsonObject>
 #include <QJsonDocument>
 #include <QJsonArray>
+#include <QThread>
 
 RobotControlDashboard::RobotControlDashboard(QWidget *parent) :
     QMainWindow(parent),
@@ -61,7 +62,10 @@ void RobotControlDashboard::launchRobot()
 {
     if ( !ui->robotNameValue->text().isEmpty() )
     {
+        QThread *thread = new QThread();
         RobotControl *robot = new RobotControl(this);
+        robot->moveToThread(thread);
+        connect(thread, &QThread::finished, robot, &QObject::deleteLater);
         launchedRobots.insert(ui->robotNameValue->text(), robot);
         robot->setIpValue(ui->ipValue->text());
         robot->setPortValue(ui->portValue->text());
@@ -84,6 +88,7 @@ void RobotControlDashboard::launchRobot()
             robot->setCameraCommands("No");
         }
         robot->show();
+        thread->start();
     }
     ui->launchButton->clearFocus();
 }
